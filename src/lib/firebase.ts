5ae521c44b5f14c86918b076055ba808943331c7
Alberto Rosales <arosaless23@gmail.com>
2025-07-11 18:43:55 +0000

    Cambiadas las credenciales a un nuevo proyecto de Firebase para solucionar errores de conexión

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDU_wlgy13AXtCLdFiXzWzUcShSw9D-m8",
-  authDomain: "sys-v2-5486e.firebaseapp.com",
-  projectId: "sys-v2-5486e",
-  storageBucket: "sys-v2-5486e.firebasestorage.app",
-  messagingSenderId: "930234849632",
-  appId: "1:930234849632:web:1320c422781617e1056605"
+  apiKey: "AIzaSyDHHSmJW3oRjPFD29At5xDePkEeGvF6Qws",
+  authDomain: "sys-v2-nuevo.firebaseapp.com",
+  projectId: "sys-v2-nuevo",
+  storageBucket: "sys-v2-nuevo.firebasestorage.app",
+  messagingSenderId: "319727149676",
+  appId: "1:319727149676:web:f783c86ca3b1e556ee6e49"
 };
 
 const app = initializeApp(firebaseConfig);
